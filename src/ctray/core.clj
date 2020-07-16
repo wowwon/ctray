7b85506f350bc98103c4d42da50d9e671ce3cd7c
Your Name <you@example.com>
2020-07-16 18:07:09 +0900

    first commit

---
+++ src/ctray/core.clj
@@ -0,0 +1,38 @@
+(ns ctray.core
+ (:require [ctray.state :refer [*pref-state *runtime-state init-pref-state!]])
+ (:import [java.awt AWTException]
+          [java.awt Image]
+          [java.awt MenuItem]
+          [java.awt PopupMenu]
+          [java.awt SystemTray]
+          [java.awt TrayIcon]
+          [java.awt.event ActionEvent]
+          [java.awt.event ActionListener]
+          [java.awt.image BufferedImage]
+          [java.io File]
+          [java.io IOException]
+          [java.util ArrayList]
+          [javax.imageio ImageIO]
+          [java.util.prefs Preferences] )
+    (:gen-class))
+(defn init[] (do
+                 (swap! *runtime-state assoc
+                      :prefs (.node (Preferences/userRoot) "ctray"))
+                 (swap! *runtime-state assoc :tray (SystemTray/getSystemTray))
+                 (let [image    (ImageIO/read (new File "./resources/TrayIcon.png" ))
+                       exit_act (reify ActionListener(actionPerformed [this e](do (prn e) (System/exit 0) ) ) )  
+                       popup_menu ( new PopupMenu)
+                       defaultItem ( new MenuItem "EXIT" )
+                       trayIcon ( new TrayIcon image  "LeinRepl" popup_menu)
+                        ]
+                              (do (swap! *runtime-state assoc :popup_menu popup_menu)
+                                  (.add popup_menu defaultItem)
+                                  (.addActionListener trayIcon exit_act)
+                                  (.add (@*runtime-state :tray) trayIcon)
+                              )
+                   )
+                 ))
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/ctray/core_test.clj
@@ -0,0 +1,7 @@
+(ns ctray.core-test
+  (:require [clojure.test :refer :all]
+            [ctray.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
